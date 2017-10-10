49ccce8933d3775481a82cf5072491534df82cce
Paul Sokolov Jr <sokolov.p64@gmail.com>
2017-10-10 20:53:45 +0300

    var -> const
    I'm not going to change these fields, right? Then let me mark it.

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/client/index.html');
 });
 app.use('/client', express.static(__dirname + '/client'));
 
+var player_list = [];
+var Player = function(
+	//new player data
+	player_id
+	
+) {
+	//set new data
+	var id = player_id;
+};
+
+io.sockets.on('connection', function(socket) {
+	socket.on('new_player', function(new_player_data) {
+		var new_player = new Player(new_player_data);
+		player_list.push(new_player);
+	});
+	//another events
+});
+
+server.listen(3000);
