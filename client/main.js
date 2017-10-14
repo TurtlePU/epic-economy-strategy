8f1f8becfdce3d6b86e284cccb392d1d0e815220
Paul Sokolov Jr <sokolov.p64@gmail.com>
2017-10-14 19:46:53 +0300

    removed on-place function declaration; added camera; started to create server-client chunk interaction.

--- client/main.js
+++ client/main.js
@@ -13,59 +13,63 @@ var gameProperties = {
 
 var main = function(game){};
 main.prototype = {
-	preload: function() {
-		game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
-		game.world.setBounds(0, 0, 
-			gameProperties.game_Width, gameProperties.game_Height, 
-			false, false, false, false);
-		game.physics.startSystem(Phaser.Physics.P2JS);
-		game.physics.p2.setBoundsToWorld(false, false, false, false, false);
-		game.physics.p2.gravity.y = 0;
-		game.physics.p2.applyGravity = false;
-	},
-	
-	create: function() {
-		game.stage.backgroundColor = 0x000000;
-		socket.on('connect', function() {
-			createPlayer();
-			gamePropeties.in_game = true;
-			socket.emit('new_player', {
-				//data of new player
-				id: socket.id
-			});
-		});
-
-		var field_group = game.add.group();
-		var bounds = game.world.getBounds()
-		var field = field_group.create(field.x, field.y, 'field');
-		
-		field.inputEnabled = true;
-		field.input.enableDrag();
-		field.events.onDragStart.add(onFieldDragStart, this);
-		field.events.onDragStop.add(onFieldDragStop, this);
-		field.events.onInputDown.add(onFieldDown, this);
-	},
-	
-	update: function() {
-		if (gameProperties.in_game) {
-			//handling commands
-
-		}
-	},
-
-	render: function() {
-		//render
-	}
+	preload: preload,
+	create: create,
+	render: render
 };
 
-function onFieldDragStart(sprite, pointer) {
+function preload() {
+	game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
+	game.world.setBounds(0, 0, 
+		gameProperties.game_Width, gameProperties.game_Height, 
+		false, false, false, false);
+	game.physics.startSystem(Phaser.Physics.P2JS);
+	game.physics.p2.setBoundsToWorld(false, false, false, false, false);
+	game.physics.p2.gravity.y = 0;
+	game.physics.p2.applyGravity = false;
+};
 
+function create() {
+	game.stage.backgroundColor = 0x000000;
+	socket.on('connect', function() {
+		createPlayer();
+		gameProperties.in_game = true;
+		socket.emit('new_player', {
+			//data of new player
+			id: socket.id
+		});
+	});
+
+	var field_group = game.add.group();
+	var bounds = game.world.getBounds()
+	var field = field_group.create(bounds.x, bounds.y, 'field');
+		
+	field.inputEnabled = true;
+	field.input.enableDrag();
+	field.events.onDragStart.add(onFieldDragStart, this);
+	field.events.onDragStop.add(onFieldDragStop, this);
+	field.events.onInputDown.add(onFieldDown, this);
+};
+
+var camera;
+function onFieldDragStart(sprite, pointer) {
+	//start dragging
 };
 function onFieldDragStop(sprite, pointer) {
-
+	//release drag
 };
 function onFieldDown(sprite, pointer) {
 
+};
+
+function request_chunks(bounds) {
+	socket.emit('chunks_requested', {
+		//
+	});
+};
+
+function render() {
+	//render
 };
 
 function createPlayer() {
@@ -76,6 +80,15 @@ var gameBootstrapper = {
 	init: function(gameContainerElementId) {
 		game.state.add('main', main);
 		game.state.start('main');
+		camera = new Phaser.Camera(game, 0, 
+			rand_in_range(innerWidth / 2, 
+				gameProperties.game_Width - innerWidth / 2),
+			rand_in_range(innerHeight / 2,
+				gameProperties.game_Height - innerHeight / 2),
+			innerWidth,
+			innerHeight
+		);
+		//maybe unfinished camera
 	}
 };
 
