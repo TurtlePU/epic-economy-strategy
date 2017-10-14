4642d197e77476dc2192ed8ae870099fa6171b88
Paul Sokolov Jr <sokolov.p64@gmail.com>
2017-10-14 21:23:17 +0300

    Finished server side in chunk interactions.

--- server.js
+++ server.js
@@ -10,17 +10,34 @@ app.use('/client', express.static(__dirname + '/client'));
 var player_list = [];
 var Player = function(
 	//new player data
-	player_id
-	
+	player_id,
+	player_spawn
 ) {
 	//set new data
 	var id = player_id;
+	var spawn = player_spawn;
 };
 
 io.sockets.on('connection', function(socket) {
-	socket.on('new_player', function(new_player_data) {
-		var new_player = new Player(new_player_data.id);
+	socket.on('new_player', function(data) {
+		var new_player = new Player(data.id, data.spawn);
 		player_list.push(new_player);
+	});
+	socket.on('chunks_requested', function(bounds) {
+		var res_list = [];
+		for (let i = bounds.topleft.y; 
+			i <= bounds.bottomright.y; ++i) {
+			let t_list = [];
+			for (let j = bounds.topleft.x;
+				j <= bouns.bottomright.x; ++j)
+				t_list.push(field[i][j]);
+			res_list.push(t_list);
+		}
+		socket.emit('chunks_received', res_list);
+	});
+	socket.on('chunk_updated_send', function(chunk) {
+		field[chunk.x][chunk.j] = chunk;
+		io.emit('chunk_updated', chunk);
 	});
 	//another events
 });
