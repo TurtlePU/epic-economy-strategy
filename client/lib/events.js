c1abe084818f627f3b40b5b7681719696cd2c35c
Paul Sokolov Jr <sokolov.p64@gmail.com>
2018-01-30 21:01:03 +0300

    this file now contains the description of all game events
    Signed-off-by: Paul Sokolov Jr <sokolov.p64@gmail.com>

---
+++ client/lib/events.js
@@ -0,0 +1,84 @@
+function EventsEnvironment(window, socket, PIXI) {
+	const EE = this,
+		  GE = new GameEnvironment(PIXI, EE);
+	
+	this.start = () => { GE.start(); };
+
+	socket.on('connect', function() {
+		console.log("connect");
+	});
+
+	this.emitPlayer = () => {
+		socket.emit('new_player', {
+			id: socket.id,
+			//maybe smth else
+		});
+	};
+
+	socket.on('gameDataSend', function(gameData) {
+		console.log("game data send");
+		GE.build(gameData);
+		addResizeListener();
+		addKeyboardMovement();
+		GE.addMouseListener();
+	});
+
+	this.emitBuild = (data) => { socket.emit('build', data); };
+	this.emitUpgradeBuilding = (data) => { socket.emit('upgrade_building'); };
+	this.emitRemoveBuilding = (data) => { socket.emit('remove_building'); };
+
+	socket.on('chunkUpdated', GE.updateChunk);
+	socket.on('resources_updated', GE.updateResources);
+
+	function keyboard(keyCode) {
+		var key = {
+			code: keyCode,
+			isDown: false,
+			isUp: true,
+			press: function() {},
+			release: function() {},
+			downHandler: function(event) {
+				if (event.keyCode === key.code) {
+					if (key.isUp && key.press) {
+						key.press();
+					}
+					key.isDown = true;
+					key.isUp = false;
+				}
+				event.preventDefault();
+			},
+			upHandler: function(event) {
+				if (event.keyCode === key.code) {
+					if (key.isDown && key.release) {
+						key.release();
+					}
+					key.isDown = false;
+					key.isUp = true;
+				}
+				event.preventDefault();
+			}
+		};
+
+		window.addEventListener("keydown", key.downHandler.bind(key), false);
+		window.addEventListener("keyup", key.upHandler.bind(key), false);
+
+		return key;
+	}
+
+	function addResizeListener() {
+		window.addEventListener("resize", GE.resize(window), false);
+	}
+
+	const keyLeft = keyboard(65), 
+		  keyRight = keyboard(68), 
+		  keyUp = keyboard(87), 
+		  keyDown = keyboard(83);
+
+	function addKeyboardMovement() {
+		var step = 3;
+		keyLeft.press = keyRight.release = GE.moveScreenByPoint(-step, 0);
+		keyRight.press = keyLeft.release = GE.moveScreenByPoint(step, 0);
+		keyUp.press = keyDown.release = GE.moveScreenByPoint(0, -step);
+		keyDown.press = keyUp.release = GE.moveScreenByPoint(0, step);	
+	}
+}
